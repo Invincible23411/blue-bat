c68df50a0d4d6234796e4fa0b6d6ab3070953ea4
Invincible23411 <87852132+Invincible23411@users.noreply.github.com>
2021-08-30 12:56:07 +0800

    Add files via upload
    open blue bat not Disco.vbs

---
+++ blue.bat
@@ -0,0 +1,42 @@
+@echo off
+start cmd.exe
+start Disco.vbs
+start cmd.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+msg * "You cant stop this"
+Timeout 10
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 3
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 3
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 3
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 3
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 3
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 3
+taskkill /f /im explorer.exe
+start explorer.exe
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 3
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 3
+msg * "The End"
