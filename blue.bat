4bcca3250fa34e212f7bd818a9ee0002cccc58ad
Invi <87852132+Invincible23411@users.noreply.github.com>
2021-09-01 09:13:25 +0800

    blue.bat
    main file!

--- blue.bat
+++ blue.bat
@@ -26,7 +26,6 @@ start calc.exe
 start calc.exe
 start calc.exe
 Timeout 5
-msg * "You cant stop this"
 Timeout 4
 taskkill /f /im explorer.exe
 start explorer.exe
@@ -109,4 +108,3 @@ Timeout 1
 taskkill /f /im explorer.exe
 start explorer.exe
 Timeout 1
-msg * "The End"
